7bc885db53938950cb9c2a99c51e092dc93233b5
AvacadoDEV <102958787+AvacadoDEV@users.noreply.github.com>
2022-04-21 00:02:42 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -4,7 +4,6 @@ module.exports = async () => {
     try {
         const connectionParams = {
             useNewUrlParser: true,
-            useCreateIndex: true,
             useUnifiedTopology: true,
         };
         await mongoose.connect(
